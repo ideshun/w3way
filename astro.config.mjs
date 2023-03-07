537f1b4943281b491c1dd03dd293359947a562ce
Deshun <liudeshun@pansoft.com>
2023-03-07 12:05:08 +0800

    feat(build): 修改配置文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
  * @Author: Deshun
  * @Date: 2023-03-06 17:03:29
  * @LastEditors: Deshun
- * @LastEditTime: 2023-03-07 12:03:08
+ * @LastEditTime: 2023-03-07 12:05:02
  * @FilePath: \w3way\astro.config.mjs
  * @Description: 文件描述
  */
@@ -17,4 +17,5 @@ export default defineConfig({
     // Enable React for the Algolia search component.
     react(),
   ],
+  site: 'https://ideshun.github.io',
 });
